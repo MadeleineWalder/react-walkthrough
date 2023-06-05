43146503cd4eaeda138ec02df32bd8fe65a7491e
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-06-05 08:50:58 +0000

    Set up first stateless function with props

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// regular stateless function
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React</h1>
+// }
+
+
+// same stateless function as an arrow function
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+
+export default FunctionalGreeting;
