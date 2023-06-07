06aedcb0faac361d676fe0f5f996305ec0440a56
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-06-07 09:50:32 +0000

    complete stateful components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"/>
+      <StatefulGreetingWithPrevState/>
     </div>
   );
 }
