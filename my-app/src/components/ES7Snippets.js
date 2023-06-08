cb3b52c00fb5c111f39ef792e9ec8fa3028a5f6b
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-06-08 09:25:47 +0000

    add snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,83 @@
+/*
+
+import React from 'react'
+
+*/
+
+/*
+
+'rfce' creates a function
+
+function ES7Snippets() {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
+
+*/
+
+/*
+
+'rafce' creates an arror function
+
+import React from 'react'
+
+const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+export default ES7Snippets
+
+*/
+
+/*
+
+'rce' creates a class component
+
+import React, { Component } from 'react'
+
+export class ES7Snippets extends Component {
+  render() {
+    return (
+      <div>ES7Snippets</div>
+    )
+  }
+}
+
+export default ES7Snippets
+
+*/
+
+/* 
+
+'rconst' creates the constructor
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+*/
+
+/*
+
+'clg' creates a console log
+
+console.log(first)
+
+*/
+
+/* 
+
+'clo' to log an object with its name as a string
+
+console.log('first', first)
+
+*/
